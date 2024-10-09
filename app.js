8ba4bbd2b56113308fe34d2d10e15c34be3d7711
Eddie Tong <edisonjtong@gmail.com>
2024-10-09 12:13:40 -0600

    adjusted form

--- app.js
+++ app.js
@@ -3,20 +3,8 @@ const menuToggle = document.getElementById("menu-toggle");
 const menu = document.getElementById("menu");
 
 menuToggle.addEventListener("click", () => {
-  menu.classList.toggle("show");
+    menu.classList.toggle("show");
 });
-
-// // JavaScript to handle active state on navbar links
-// const navLinks = document.querySelectorAll(".option");
-// navLinks.forEach((link) => {
-//   link.addEventListener("click", function () {
-//     // Remove 'active' class from all links
-//     navLinks.forEach((link) => link.classList.remove("active"));
-
-//     // Add 'active' class to the clicked link
-//     this.classList.add("active");
-//   });
-// });
 
 // Retrieve the last clicked navbar option from localStorage
 const lastActive = localStorage.getItem("activeLink");
@@ -24,16 +12,16 @@ const navLinks = document.querySelectorAll(".option");
 
 // Set the active class based on the stored value
 if (lastActive) {
-  navLinks.forEach((link) => {
-    if (link.getAttribute("href") === lastActive) {
-      link.classList.add("active");
-    }
-  });
+    navLinks.forEach((link) => {
+        if (link.getAttribute("href") === lastActive) {
+            link.classList.add("active");
+        }
+    });
 }
 
 // Add event listeners to save the clicked link to localStorage
 navLinks.forEach((link) => {
-  link.addEventListener("click", function () {
-    localStorage.setItem("activeLink", this.getAttribute("href"));
-  });
+    link.addEventListener("click", function () {
+        localStorage.setItem("activeLink", this.getAttribute("href"));
+    });
 });
